5bc9738bb475fcc529cecf10b5d2fe4e7efb8083
Space Automation <no-reply@automation.jetbrains.space>
2026-07-29 19:55:17 +0000

    Update Kotlin Toolchain version to 0.12.0-dev-4187

--- examples/spring-petclinic/kotlin.bat
+++ examples/spring-petclinic/kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4179
+set kotlin_cli_version=0.12.0-dev-4187
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=bc94db04daccb39f7c0a659bd167e77e17ba5ecad6f3dfd51f343e6ad9e49d21
+set kotlin_cli_sha256=3e399ffc418323c19e8a796905dc2fe93a8e7e9adbc9b94650c806205dc7492c
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
