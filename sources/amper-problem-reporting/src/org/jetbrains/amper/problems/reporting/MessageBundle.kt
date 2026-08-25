f8ea6dd4ba11819635cf1f049d7d9c2180a475d2
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2026-08-25 14:54:22 +0200

    [problem-reporting] Use bundle's class to load the resource
    If submodule with the bundle is registered in a classloader different from the one that loaded `MessageBundle`, the resource can't be found.
    
    This is the case for the IntelliJ plugin where Android libraries are loaded in the Android-specific content module, while the MessageBundle itself is loaded in the parent's core content module.

--- sources/amper-problem-reporting/src/org/jetbrains/amper/problems/reporting/MessageBundle.kt
+++ sources/amper-problem-reporting/src/org/jetbrains/amper/problems/reporting/MessageBundle.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.problems.reporting
@@ -9,7 +9,13 @@ import java.text.MessageFormat
 import java.util.*
 
 open class MessageBundle(bundleName: String) {
-    private val resourceBundle = ResourceBundle.getBundle(bundleName)
+    private val resourceBundle = ResourceBundle.getBundle(
+        bundleName,
+        Locale.getDefault(),
+        // The default uses the caller's class loader, which doesn't work nicely if the bundle itself was loaded in
+        // a different class loader (e.g., in content modules of the IntelliJ plugin).
+        this::class.java.classLoader,
+    )
 
     fun message(messageKey: String, vararg arguments: Any?): @Nls String {
         if (!resourceBundle.containsKey(messageKey)) {
