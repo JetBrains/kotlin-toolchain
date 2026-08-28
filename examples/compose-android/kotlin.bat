e36bbe6340e84711d7ff35415c79a442354aba93
Space Automation <no-reply@automation.jetbrains.space>
2026-08-28 19:14:53 +0000

    Update Kotlin Toolchain version to 0.13.0-dev-4325

--- examples/compose-android/kotlin.bat
+++ examples/compose-android/kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.13.0-dev-4312
+set kotlin_cli_version=0.13.0-dev-4325
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=5fb070adfbec005050793ebe0265f862e595f6fe1d35c50ae1a11ca1136cd8a8
+set kotlin_cli_sha256=b4b86f4e5ab1f0f925c547cdfad808eaa3bde0138ac0d1c5bd53f346f6137e86
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
